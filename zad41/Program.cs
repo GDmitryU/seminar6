01b61e577653918dca70e0e24f3d1f0d4854ae33
DmitryG <dmitrygribchenko@mail.ru>
2022-09-18 18:05:00 +0300

    zad41

---
+++ zad41/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int AmountPositiveNumbers(int M)
+
+{
+int count = 0;   
+ for (int i=1;i<= M;i++)
+     {
+        Console.Write($"Введите  {i} число ");
+        int positive = int.Parse(Console.ReadLine());
+    
+         if (positive > 0) 
+         {
+            count+=1;
+         }
+        
+    }
+
+    return count;
+}
+    
+Console.Write("Введите количество чисел ");
+int amountnumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество положительных элементов массива = {AmountPositiveNumbers( amountnumber)}");    
+
+
